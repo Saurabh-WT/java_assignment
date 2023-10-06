cc2fff31cc4cbd0c0d1efe17175bccf6c4d436cc
fiza malik <fiza.malik@walkingtree.tech>
2023-10-06 19:09:57 +0530

    day 11 and 12

--- Fiza/src/day11_12/Ex9.java
+++
@@ -1,9 +0,0 @@
-package day11_12;
-//9. Finally Block Usage:
-//Write a program that reads data from a database.
-// Use a try-catch-finally block to ensure that database resources are properly closed, even if an exception occurs.
-public class Ex9 {
-    public static void main(String[] args) {
-
-    }
-}
