5dca31e7cd393a17c19b77107c66f65caaf3ef0e
fiza malik <fiza.malik@walkingtree.tech>
2023-10-06 19:11:13 +0530

    day 11 and 12

--- Fiza/src/day11_12/Ex9.java
+++ Fiza/src/day11_12/Ex9.java
@@ -8,7 +8,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-public class MySQLDatabaseReadExample {
+public class Ex9 {
 
     public static void main(String[] args) {
         Connection connection = null;
